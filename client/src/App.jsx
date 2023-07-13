8a3b4bf1f55b35752a5b757b27c119cec7b0369d
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-13 22:24:14 +0530

    logo updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,10 +8,10 @@ const App = () => {
     <BrowserRouter>
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b[#e6ebf4]'>
       <Link to='/'>
-        <img src={logo} alt='logo' className='w-28  object-contain'/>
+        <div className='flex items-center'><img src={logo} alt='logo' className='w-28  object-contain'/><h1 className='font-inter text-4xl font-[650] '>AIComicHub</h1></div>
       </Link>
 
-      <Link to='/create-post' className='font-inter font-medium bg-[#6469ff]  text-white px-4 py-2 rounded-md'>Create</Link>
+      <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
       </header>
 
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
