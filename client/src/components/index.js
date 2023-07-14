0b0cda73a08f750460d1f959d9f12c64437579dc
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-14 16:32:41 +0530

    add carousel export

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Card from './Card';
 import FormField from './FormField';
 import Loader from './Loader';
+import CarouselWidget from './Carousel';
 
-export { Card, FormField, Loader };
+export { Card, FormField, Loader,CarouselWidget };
