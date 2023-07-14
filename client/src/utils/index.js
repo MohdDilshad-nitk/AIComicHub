3516be3d30ca56b3e4370b652978c0eddbcd55f3
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-14 16:34:05 +0530

    download the comic in form of zip

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,7 @@
 import {surpriseMePrompts} from '../constants'
 import FileSaver from 'file-saver';
-
+import JSZip from 'jszip';
+import fs from 'fs';
 export const getRandomPrompt = (prompt) => {
     const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
     const randomPrompt  = surpriseMePrompts[randomIndex];
@@ -9,6 +10,37 @@ export const getRandomPrompt = (prompt) => {
 }
 
 
-export async function downloadImage(_id, photo) {
-    FileSaver.saveAs(photo, `download-${_id}.jpg`);
+export async function downloadImage(_id,photos) {
+
+
+    try {
+        const response = await fetch('http://localhost:8080/api/v1/urlTobase', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            photos: photos,
+          }),
+        });
+  
+        if (response.ok) {
+          const result = await response.json();
+          const zip = new JSZip();
+          result.forEach((photo,idx) => {
+            zip.file(`Image-${idx}.png`,photo,{base64: true});
+            });
+
+             zip.generateAsync({type:"blob"}).then(function(content) {
+                // see FileSaver.js
+                FileSaver.saveAs(content, `download-${_id}.zip`);
+            });
+        
+
+        }
+      } catch (err) {
+        alert(err);
+      } 
+
+    
   }
