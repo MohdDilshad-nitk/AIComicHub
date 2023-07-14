fb5037ccc0ebddee671e8f336a2aaf8b0ed340ab
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-14 16:32:05 +0530

    Card component to display image

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import {download} from '../assets';
-import {downloadImage} from '../utils';
 
-const Card = ({_id,name,prompt,photo}) => {
-  return (
-    <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
-      <img className='w-full h-auto object-cover rounded-xl ' src={photo} alt={prompt}/>
-      <div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md'>
-      <p className="text-white text-md overflow-y-auto prompt">{prompt}</p>
+import { download } from '../assets';
+import { downloadImage } from '../utils';
+
+const Card = ({ _id, name, prompt, photo }) => (
+  <div className="rounded-xl h-full w-full group relative shadow-card hover:shadow-cardhover card">
+    <img
+      className="w-full h-auto object-cover rounded-xl"
+      src={photo}
+      alt={prompt}
+    />
+    <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+      <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
 
       <div className="mt-5 flex justify-between items-center gap-2">
         <div className="flex items-center gap-2">
@@ -18,10 +22,8 @@ const Card = ({_id,name,prompt,photo}) => {
           <img src={download} alt="download" className="w-6 h-6 object-contain invert" />
         </button>
       </div>
-
-      </div>
     </div>
-  )
-}
+  </div>
+);
 
-export default Card
+export default Card;
