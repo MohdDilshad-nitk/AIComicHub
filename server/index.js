7ede9cfad95fcb3f1650919dc33a9d28aad64bdd
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-14 16:41:31 +0530

    route to convert url to base64 images

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
+import urlToBase from './routes/urltoBase64.js'
 dotenv.config();
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json({limit:'50mb'}));
 
 app.use('/api/v1/post',postRoutes);
 app.use('/api/v1/dalle',dalleRoutes);
+app.use('/api/v1/urlTobase',urlToBase);
 
 const port = 8080
 
