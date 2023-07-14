bc609473426b0f64ebd74456966932e57f6fca11
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-14 16:42:02 +0530

    updated the post from string to list of string

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -3,8 +3,14 @@ import mongoose from "mongoose";
 const Post = new mongoose.Schema({
     name : {type : String, required : true},
     prompt : {type : String, required : true},
-    photo : {type : String, required : true}
-})
+    photos : {type : [String], required : true,validate: {
+        validator: function (array) {
+          return array.length > 0;
+        },
+        message: 'At least one photo should be there',
+      },}
+});
+
 
 const PostSchema = mongoose.model('Post',Post);
 
